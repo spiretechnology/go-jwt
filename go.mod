db168e91e0f041b7d68c160478a43c866db05c56
connerdouglass <conner.douglass@gmail.com>
2022-05-07 02:10:47 -0400

    Bump major version number in go.mod

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/spiretechnology/go-jwt
+module github.com/spiretechnology/go-jwt/v2
 
 go 1.18
 
--- token_test.go
+++ token_test.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/spiretechnology/go-jwt"
+	"github.com/spiretechnology/go-jwt/v2"
 	"github.com/stretchr/testify/assert"
 )
 
